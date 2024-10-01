659f6834dc872664d5b9bd13226b15388225df80
sophia <scarazoortiz@berkeley.edu>
2024-10-01 05:45:50 -0700

    need ending

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//if you click button do this
+function next(current, nextone) {
+    document.getElementById(current).style.display = "none";
+    document.getElementById(nextone).style.display = "block";
+}
+let tracker = 0;
+
+function scorecount(change) {
+   tracker += change
+}
